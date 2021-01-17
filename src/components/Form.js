63e69068f28dd1be554b4d687d209590fe1bb8db
qadir agha <abdulqadiragha@gmail.com>
2021-01-17 12:41:01 +0430

    back to fixed virsion

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,22 +5,21 @@ import Result from './Result'
 class Form extends Component{
     constructor(){
         super()
-        this.state = {firstPerson: "", secondPerson:"",tag:"", result:{}}
+        this.state = {firstPerson: "", secondPerson:"", result:{}}
         this.formSubmit = this.formSubmit.bind(this)
         this.handleInput = this.handleInput.bind(this)
     }
 
     formSubmit(event) {
-        fetch(`https://adult-movie-provider.p.rapidapi.com/api/video/FindVideo?keyword=${this.state.tag}&offset=0&next=10`, {
+        fetch(`https://love-calculator.p.rapidapi.com/getPercentage?fname=${this.state.firstPerson}&sname=${this.state.secondPerson}`, {
             "method": "GET",
             "headers": {
                 "x-rapidapi-key": "4a6a62fd82msha81718d72bd9741p15c630jsn02024f4b11a5",
-                "x-rapidapi-host": "adult-movie-provider.p.rapidapi.com"
+                "x-rapidapi-host": "love-calculator.p.rapidapi.com"
             }
         })
         .then(response => response.json()
         ).then(result => {
-            let thumbs = result.thumbs(el => <img src={el} alt="none"></img>)
             this.setState({firstPerson: "", secondPerson:"", result:result})
         })
         .catch(err => {
@@ -57,7 +56,7 @@ class Form extends Component{
                     </div>
                 </div>
             </div> 
-            { Object.keys(this.state.result).length > 0 && < Result data={this.state} />} 
+            { Object.keys(this.state.result).length > 0 && < Result data={this.state.result} />} 
         </div>
         )
     }
